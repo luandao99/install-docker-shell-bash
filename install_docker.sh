3a397f0321b1b48c350d968b3b339e08ef3425d4
luandao99 <82952863+luandao99@users.noreply.github.com>
2023-10-25 06:07:32 +0000

    Install docker-done

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
 
-# This is a simple shell script
-echo "Hello, World!"
+# Cập nhật danh sách các gói
+sudo apt-get update
+if [ $? -ne 0 ]; then
+  echo "Lỗi trong quá trình cập nhật danh sách gói."
+  exit 1
+fi
 
-# You can add more commands here
+# Cài đặt các gói phụ thuộc
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Tạo thư mục /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings
+
+# Tải khóa GPG của Docker và lưu nó vào /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Thêm nguồn dữ liệu Docker vào /etc/apt/sources.list.d/docker.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Cập nhật danh sách các gói lại sau khi đã thêm nguồn dữ liệu Docker
+sudo apt-get update
+
+# Cài đặt Docker và Docker Compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+# Kiểm tra xem Docker đã được cài đặt thành công hay không
+if [ -x "$(command -v docker)" ]; then
+  echo "Docker tải thành công."
+else
+  echo "Docker tải không thành công."
+fi
+
+# Kiểm tra phiên bản của Docker
+docker -v
+# Kiểm tra xem Docker đã được cài đặt thành công hay không
+if [ -x "$(command -v docker-compose)" ]; then
+  echo "Docker Compose tải thành công."
+else
+  echo "Docker Compose tải không thành công."
+fi
+
+# Kiểm tra phiên bản của Docker
+docker-compose -v
+exit 0
